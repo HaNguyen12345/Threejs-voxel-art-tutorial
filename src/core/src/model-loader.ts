eb32560ea2b85e6a07bcb000244ad0060e31a95b
Nguyễn Tiến Anh <96630540+acocmor@users.noreply.github.com>
2024-03-28 15:15:40 +0700

    Change algorithm create voxel

--- src/core/src/model-loader.ts
+++ src/core/src/model-loader.ts
@@ -6,7 +6,7 @@ import { RoundedBoxGeometry } from "three/examples/jsm/geometries/RoundedBoxGeom
 
 const params = {
 	gridSize: 0.15,
-	boxSize: 0.1,
+	boxSize: 0.15,
 	boxRoundness: 0.01
 }
 
@@ -237,128 +237,35 @@ export class ModelLoader {
 			}
 		});
 
-		let boundingBox: any = new THREE.Box3().setFromObject(importedScene);
+		let modelVoxels: any = [];
 
-		let modelVoxels: any = [],
-			inItemX: any = [],
-			inItemY: any = [],
-			inItemZ: any = [],
-			inPointX: any = [],
-			inPointY: any = [],
-			inPointZ: any = []
+		for (const mesh of importedMeshes) {
+			const boundingBox = new THREE.Box3().setFromObject(mesh);
 
-		for (let i = boundingBox.min.x; i <= boundingBox.max.x + params.gridSize; i += params.gridSize) {
-			for (let j = boundingBox.min.y; j <= boundingBox.max.y + params.gridSize; j += params.gridSize) {
-				for (let k = boundingBox.min.z; k <= boundingBox.max.z + params.gridSize; k += params.gridSize) {
-					for (let meshCnt = 0; meshCnt < importedMeshes.length; meshCnt++) {
-						const mesh = importedMeshes[meshCnt];
-						const pos = new THREE.Vector3(i, j, k);
+			// Get size data
+			const gridSize = params.gridSize;
+			const boxSize = params.boxSize;
+
+			for (let x = boundingBox.min.x - gridSize; x <= boundingBox.max.x + gridSize; x += gridSize) {
+				for (let y = boundingBox.min.y - gridSize; y <= boundingBox.max.y + gridSize; y += gridSize) {
+					for (let z = boundingBox.min.z - gridSize; z <= boundingBox.max.z + gridSize; z += gridSize) {
+						// get position form center of voxel block
+						const centerPoint = new THREE.Vector3(x + boxSize / 2, y + boxSize / 2, z + boxSize / 2);
 
 						if (
-							this.isInsideMesh(pos, new THREE.Vector3(0, -1, 0), mesh)
-							|| this.isInsideMesh(pos, new THREE.Vector3(1, 0, 0), mesh)
-							|| this.isInsideMesh(pos, new THREE.Vector3(0, 1, 0), mesh)
-							|| this.isInsideMesh(pos, new THREE.Vector3(0, 0, 1), mesh)
-							|| this.isInsideMesh(pos, new THREE.Vector3(-1, 0, 0), mesh)
-							|| this.isInsideMesh(pos, new THREE.Vector3(0, 0, -1), mesh)
+							this.isInsideMesh(centerPoint, new THREE.Vector3(0, -1, 0), mesh)
+							|| this.isInsideMesh(centerPoint, new THREE.Vector3(1, 0, 0), mesh)
+							|| this.isInsideMesh(centerPoint, new THREE.Vector3(0, 1, 0), mesh)
+							|| this.isInsideMesh(centerPoint, new THREE.Vector3(0, 0, 1), mesh)
+							|| this.isInsideMesh(centerPoint, new THREE.Vector3(-1, 0, 0), mesh)
+							|| this.isInsideMesh(centerPoint, new THREE.Vector3(0, 0, -1), mesh)
 						) {
-							modelVoxels.push({ position: pos });
-							break;
+							modelVoxels.push({ position: centerPoint });
 						}
 					}
 				}
 			}
 		}
-		// X
-		modelVoxels.forEach((item: any) => {
-			const isExistPoint = inItemX.find((inItem: any) => inItem.z === item.position.z && inItem.y === item.position.y)
-			if (isExistPoint) {
-				inItemX[inItemX.indexOf(isExistPoint)].listPoint.push(item)
-			} else {
-				inItemX.push({
-					z: item.position.z,
-					y: item.position.y,
-					listPoint: [item]
-				})
-			}
-		})
-
-		inItemX.forEach((inItem: any) => {
-			inItem.listPoint?.forEach((currentPoint: any, currentIndexPoint: any, listPoint: any) => {
-				if (currentIndexPoint > 0 && (currentPoint.position.x - listPoint[currentIndexPoint - 1].position.x > params.gridSize)) {
-					for (let i = listPoint[currentIndexPoint - 1].position.x + params.gridSize; i < currentPoint.position.x; i += params.gridSize) {
-						const pos = new THREE.Vector3(i, currentPoint.position.y, currentPoint.position.z);
-						inPointX.push({ position: pos })
-					}
-				}
-			})
-		})
-
-		// Y
-		modelVoxels.forEach((item: any) => {
-			const isExistPoint = inItemY.find((inItem: any) => inItem.x === item.position.x && inItem.z === item.position.z)
-			if (isExistPoint) {
-				inItemY[inItemY.indexOf(isExistPoint)].listPoint.push(item)
-			} else {
-				inItemY.push({
-					x: item.position.x,
-					z: item.position.z,
-					listPoint: [item]
-				})
-			}
-		})
-
-		inItemY.forEach((inItem: any) => {
-			inItem.listPoint?.forEach((currentPoint: any, currentIndexPoint: any, listPoint: any) => {
-				if (currentIndexPoint > 0 && (currentPoint.position.y - listPoint[currentIndexPoint - 1].position.y > params.gridSize)) {
-					for (let i = listPoint[currentIndexPoint - 1].position.y + params.gridSize; i < currentPoint.position.y; i += params.gridSize) {
-						const pos = new THREE.Vector3(currentPoint.position.x, i, currentPoint.position.z);
-						inPointY.push({ position: pos })
-					}
-				}
-			})
-		})
-
-		// Z
-		modelVoxels.forEach((item: any) => {
-			const isExistPoint = inItemZ.find((inItem: any) => inItem.x === item.position.x && inItem.y === item.position.y)
-			if (isExistPoint) {
-				inItemZ[inItemZ.indexOf(isExistPoint)].listPoint.push(item)
-			} else {
-				inItemZ.push({
-					x: item.position.x,
-					y: item.position.y,
-					listPoint: [item]
-				})
-			}
-		})
-
-		inItemZ.forEach((inItem: any) => {
-			inItem.listPoint?.forEach((currentPoint: any, currentIndexPoint: any, listPoint: any) => {
-				if (currentIndexPoint > 0 && (currentPoint.position.z - listPoint[currentIndexPoint - 1].position.z > params.gridSize)) {
-					for (let i = listPoint[currentIndexPoint - 1].position.z + params.gridSize; i < currentPoint.position.z; i += params.gridSize) {
-						const pos = new THREE.Vector3(currentPoint.position.x, currentPoint.position.y, i);
-						inPointZ.push({ position: pos })
-					}
-				}
-			})
-		})
-
-		var map1 = this.arrayToMap(inPointX);
-		var map2 = this.arrayToMap(inPointY);
-		var map3 = this.arrayToMap(inPointZ);
-
-		for (var key in map1) {
-			if (map2[key] && map3[key]) {
-				modelVoxels.push({...JSON.parse(key), color: new THREE.Color().setHSL(.9, .9, .9)})
-			}
-		}
-
-		// let min = new THREE.Vector3(boundingBox.min.x, boundingBox.min.y, boundingBox.min.z)
-		// let max = new THREE.Vector3(boundingBox.max.x, boundingBox.max.y, boundingBox.max.z)
-
-		// modelVoxels.push({ position: min, color: new THREE.Color().setHSL(.6, .6, .6) });
-		// modelVoxels.push({ position: max, color: new THREE.Color().setHSL(.6, .6, .6) });
 
 		return modelVoxels
 	}
@@ -368,7 +275,7 @@ export class ModelLoader {
 		rayCaster.set(pos, ray);
 		let rayCasterIntersects = rayCaster.intersectObject(mesh, false);
 		// we need odd number of intersections
-		return rayCasterIntersects.length % 2 === 1 && rayCasterIntersects[0].distance <= params.gridSize;
+		return rayCasterIntersects.length > 0 && rayCasterIntersects[0].distance <= params.gridSize;
 	}
 
 	private recreateInstancedMesh(array: any, cnt: any) {
